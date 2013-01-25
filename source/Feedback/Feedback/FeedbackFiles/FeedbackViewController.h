2a0c6342af806f322689cd2b3e02c6e2dbd8293d
Chris Risner <github@chrisrisner.com>
2013-01-25 13:25:36 -0800

    Update

--- source/Feedback/Feedback/FeedbackFiles/FeedbackViewController.h
+++ source/Feedback/Feedback/FeedbackFiles/FeedbackViewController.h
@@ -8,7 +8,7 @@
 
 #import <UIKit/UIKit.h>
 
-@interface FeedbackViewController : UIViewController <UITextViewDelegate> {
+@interface FeedbackViewController : UIViewController <UITextViewDelegate, UITextFieldDelegate> {
     @private
     int maxCharacters;
 }
@@ -17,5 +17,19 @@
 
 @property (weak, nonatomic) IBOutlet UITextView *txtComments;
 @property (weak, nonatomic) IBOutlet UILabel *lblCharsRemaining;
+@property (weak, nonatomic) IBOutlet UITextField *txtEmailAddress;
+- (IBAction)tappedStarOne:(id)sender;
+- (IBAction)tappedStarTwo:(id)sender;
+- (IBAction)tappedStarThree:(id)sender;
+- (IBAction)tappedStarFour:(id)sender;
+- (IBAction)tappedStarFive:(id)sender;
+
+@property (weak, nonatomic) IBOutlet UIButton *btnStarOne;
+@property (weak, nonatomic) IBOutlet UIButton *btnStarTwo;
+@property (weak, nonatomic) IBOutlet UIButton *btnStarThree;
+@property (weak, nonatomic) IBOutlet UIButton *btnStarFour;
+@property (weak, nonatomic) IBOutlet UIButton *btnStarFive;
+@property (weak, nonatomic) IBOutlet UIScrollView *svScrollView;
+
 
 @end
--- source/Feedback/Feedback/FeedbackFiles/FeedbackViewController.m
+++ source/Feedback/Feedback/FeedbackFiles/FeedbackViewController.m
@@ -55,6 +55,17 @@
     } else {
         self.lblCharsRemaining.hidden = YES;
     }
+    
+    UITapGestureRecognizer *tap = [[UITapGestureRecognizer alloc]
+                                   initWithTarget:self
+                                   action:@selector(dismissKeyboard)];
+    
+    [self.view addGestureRecognizer:tap];
+}
+
+-(void)dismissKeyboard {
+    [self.txtEmailAddress resignFirstResponder];
+    [self.txtComments resignFirstResponder];
 }
 
 - (void)didReceiveMemoryWarning
@@ -79,4 +90,80 @@
     }
 }
 
+- (IBAction)tappedStarOne:(id)sender {
+    [self setRatingWithStars:1];
+}
+
+- (IBAction)tappedStarTwo:(id)sender {
+    [self setRatingWithStars:2];
+}
+
+- (IBAction)tappedStarThree:(id)sender {
+    [self setRatingWithStars:3];
+}
+
+- (IBAction)tappedStarFour:(id)sender {
+    [self setRatingWithStars:4];
+}
+
+- (IBAction)tappedStarFive:(id)sender {
+    [self setRatingWithStars:5];
+}
+
+-(void) setRatingWithStars:(int)starCount {
+    //Dismiss the keyboard just in case it's showing
+    [self dismissKeyboard];
+    UIImage *selectedStarImage = [UIImage imageNamed:@"SelectedStar.png"];
+    UIImage *unselectedStarImage = [UIImage imageNamed:@"UnselectedStar.png"];
+    
+    if (starCount == 1) {
+        [self.btnStarOne setBackgroundImage:selectedStarImage forState: UIControlStateSelected & UIControlStateApplication];
+        [self.btnStarTwo setBackgroundImage:unselectedStarImage forState:UIControlStateSelected & UIControlStateApplication];
+        [self.btnStarThree setBackgroundImage:unselectedStarImage forState:UIControlStateSelected & UIControlStateApplication];
+        [self.btnStarFour setBackgroundImage:unselectedStarImage forState:UIControlStateSelected & UIControlStateApplication];
+        [self.btnStarFive setBackgroundImage:unselectedStarImage forState:UIControlStateSelected & UIControlStateApplication];
+    } else if (starCount == 2) {
+        [self.btnStarOne setBackgroundImage:selectedStarImage forState:UIControlStateSelected & UIControlStateApplication];
+        [self.btnStarTwo setBackgroundImage:selectedStarImage forState:UIControlStateSelected & UIControlStateApplication];
+        [self.btnStarThree setBackgroundImage:unselectedStarImage forState:UIControlStateSelected & UIControlStateApplication];
+        [self.btnStarFour setBackgroundImage:unselectedStarImage forState:UIControlStateSelected & UIControlStateApplication];
+        [self.btnStarFive setBackgroundImage:unselectedStarImage forState:UIControlStateSelected & UIControlStateApplication];
+    } else if (starCount == 3) {
+        [self.btnStarOne setBackgroundImage:selectedStarImage forState:UIControlStateSelected & UIControlStateApplication];
+        [self.btnStarTwo setBackgroundImage:selectedStarImage forState:UIControlStateSelected & UIControlStateApplication];
+        [self.btnStarThree setBackgroundImage:selectedStarImage forState:UIControlStateSelected & UIControlStateApplication];
+        [self.btnStarFour setBackgroundImage:unselectedStarImage forState:UIControlStateSelected & UIControlStateApplication];
+        [self.btnStarFive setBackgroundImage:unselectedStarImage forState:UIControlStateSelected & UIControlStateApplication];
+    } else if (starCount == 4) {
+        [self.btnStarOne setBackgroundImage:selectedStarImage forState:UIControlStateSelected & UIControlStateApplication];
+        [self.btnStarTwo setBackgroundImage:selectedStarImage forState:UIControlStateSelected & UIControlStateApplication];
+        [self.btnStarThree setBackgroundImage:selectedStarImage forState:UIControlStateSelected & UIControlStateApplication];
+        [self.btnStarFour setBackgroundImage:selectedStarImage forState:UIControlStateSelected & UIControlStateApplication];
+        [self.btnStarFive setBackgroundImage:unselectedStarImage forState:UIControlStateSelected & UIControlStateApplication];
+    } else if (starCount == 5) {
+        [self.btnStarOne setBackgroundImage:selectedStarImage forState:UIControlStateSelected & UIControlStateApplication];
+        [self.btnStarTwo setBackgroundImage:selectedStarImage forState:UIControlStateSelected & UIControlStateApplication];
+        [self.btnStarThree setBackgroundImage:selectedStarImage forState:UIControlStateSelected & UIControlStateApplication];
+        [self.btnStarFour setBackgroundImage:selectedStarImage forState:UIControlStateSelected & UIControlStateApplication];
+        [self.btnStarFive setBackgroundImage:selectedStarImage forState:UIControlStateSelected & UIControlStateApplication];
+    }
+}
+
+- (BOOL) textFieldShouldReturn:(UITextField *)textField {
+    [textField resignFirstResponder];
+    return YES;
+}
+
+- (void)textFieldDidBeginEditing:(UITextField *)textField {
+    if (self.svScrollView) {
+//        [self.svScrollView scrollRectToVisible:textField.frame animated:YES];
+        
+        CGRect rectBottom = CGRectZero;
+        rectBottom.size = self.svScrollView.frame.size;
+        rectBottom.origin.y = self.svScrollView.contentSize.height - rectBottom.size.height;
+        rectBottom.origin.x = 0;
+        
+        [self.svScrollView scrollRectToVisible:rectBottom animated:YES];
+    }
+}
 @end
